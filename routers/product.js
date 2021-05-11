ca20a7f5d0c4dc49ac547db4b736f95872d2921c
haida <Haidariano@hotmail.com>
2021-05-11 20:26:12 +0200

    Test virker igen

--- routers/product.js
+++ routers/product.js
@@ -1,57 +1,44 @@
 const express = require('express')
 const fs = require('fs')
 const Category = require('../models/category')
-const Product=require('../models/product')
-const router=express.Router()
+const Product = require('../models/product')
+const router = express.Router()
 const mongoose = require('mongoose')
 const multer = require('multer')
-const path=require('path')
-const { updateOne } = require('../models/category')
+const path = require('path')
 
 const fileTypes = {
-    'image/png':'png',
-    'image/jpeg':'jpeg',
-    'image/jpg':'jpg'
+    'image/png': 'png',
+    'image/jpeg': 'jpeg',
+    'image/jpg': 'jpg'
 }
 
-const fileFilter=(req,file,cb)=>{
-    const isValid = fileTypes[file.mimetype]
-    if(isValid ) {cb(null,true)}
-    else {cb(null,false)}
-}
-
- const storage = multer.diskStorage({
+const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, ('uploads/') )
+        const isValid = fileTypes[file.mimetype]
+        let uploadError = new Error('invalid image type')
+        if (isValid) {
+            uploadError = null
+        }
+        cb(uploadError, path.join(__dirname, '/uploads/imgs/'))
     },
     filename: function (req, file, cb) {
 
-      const filename = file.originalname.split(' ').join('-')  
-      const extension = fileTypes[file.mimetype]
-      const now = Date.now().toString()
-      
-      cb(null, `${filename}-${now.replace(/:/g, '-')}.${extension}`)
+        const filename = file.originalname.split(' ').join('-')
+        const extension = fileTypes[file.mimetype]
+        const now = Date.now().toString()
+
+        cb(null, `${filename}-${now.replace(/:/g, '-')}.${extension}`)
     }
 })
 
-
-const upload = multer({storage : storage, fileFilter:fileFilter})
-
+const uploadOptions = multer({storage: storage})
 
 
 //add a product
-router.post('/',upload.single('image'),async(req,res,next)=>{
-    //make sure the user has sendt product image
-    if(!req.file) return res.status(400).send('insert an image for product')
-
-    //make a path for public path for image from root
-    const filename = req.file.filename
-    const originalPath = `${req.protocol}://${req.get('host')}/uploads`
-
-    console.log("req.file : ")
-    console.log(req.file)
-     const category = await Category.findById(req.body.category).then(category =>{
-        if(!category){
+router.post('/', uploadOptions.single('image'), async (req, res, next) => {
+    const category = await Category.findById(req.body.category).then(category => {
+        if (!category) {
             return res.status(500).send('invalid category ')
         }
     }).catch(err => {
@@ -63,68 +50,24 @@ router.post('/',upload.single('image'),async(req,res,next)=>{
     console.log(fileName)
     const basePath = `${req.protocol}://${req.get('host')}/uploads/imgs/`
     console.log(basePath)
-    let product= new Product({
-        name:req.body.name,
-        description : req.body.description,
-        richDescription : req.body.richDescription,
-        image: `${originalPath}/${filename}`,
-        brand:req.body.brand,
-        price:req.body.price,
-        category:req.body.category,
-        countInStock:req.body.countInStock,
-        rating:req.body.rating,
-        numReviews:req.body.numReviews,
-        isFeatured:req.body.isFeatured
+    const product = new Product({
+        name: req.body.name,
+        description: req.body.description,
+        ricDescription: req.body.ricDescription,
+        image: `${basePath}${fileName}`,
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured
     })
-    product = await product.save().then(product =>{
-        if(!product){
+    product = await product.save().then(product => {
+        if (!product) {
             return res.status(500).send('product cannot be added')
-        }else {
+        } else {
             return res.send(product)
-        }
-    }).catch(err =>{
-        console.log(err)
-    })
-    
-})
-//get all products
-router.get('/',async (req,res)=>{
-    console.log(" req : ")
-
-    console.log(req.body)
-        const productList= await Product.find().populate('category')
-    if(!productList){
-        res.status(500).json({success:false})
-    }
-    else{
-        res.status(200).json({
-            message:'products fetched succesfully',
-            products:productList
-        })
-    }
-});
-//get product by id
-router.get('/:id',async (req,res)=>{
-    const productList= await Product.findById(req.params.id).populate('category')
-    if(!productList){
-        res.status(500).json({success:false})
-    }
-    else{
-        res.status(200).json({
-            message:'products fetched succesfully',
-            products:productList
-        })
-    }
-});
-//updating product
-router.put('/:id',upload.single('image'),async(req, res)=>{
-
-    if(!mongoose.isValidObjectId(req.params.id)){
-        return res.status(500).send('invalid productId ')
-    }
-    const category = await Category.findById(req.body.category).then(category =>{
-        if(!category){
-            return res.status(500).send('invalid category ')
         }
     }).catch(err => {
         res.status(500).json({
@@ -132,129 +75,122 @@ router.put('/:id',upload.single('image'),async(req, res)=>{
         })
     })
 
-    pro = await Product.findById(req.params.id)
-    if(!pro) return res.status(400).send('not a valid product')
-
-
-
-    const product=await Product.findByIdAndUpdate(
+})
+//get all products
+router.get('/', async (req, res) => {
+    const productList = await Product.find().populate('category')
+    if (!productList) {
+        res.status(500).json({success: false})
+    } else {
+        res.status(200).json({
+            message: 'products fetched succesfully',
+            products: productList
+        })
+    }
+});
+//get product by id
+router.get('/:id', async (req, res) => {
+    const productList = await Product.findById(req.params.id).populate('category')
+    if (!productList) {
+        res.status(500).json({success: false})
+    } else {
+        res.status(200).json({
+            message: 'products fetched succesfully',
+            products: productList
+        })
+    }
+});
+//updating product
+router.put('/:id', async (req, res) => {
+    if (!mongoose.isValidObjectId(req.params.id)) {
+        return res.status(500).send('invalid productId ')
+    }
+    const category = await Category.findById(req.body.category).then(category => {
+        if (!category) {
+            return res.status(500).send('invalid category ')
+        }
+    }).catch(err => {
+        res.status(500).json({
+            error: err
+        })
+    })
+    const product = await Product.findByIdAndUpdate(
         req.params.id,
         {
-        name:req.body.name,
-        description : req.body.description,
-        ricDescription : req.body.ricDescription,
-        image:req.body.image,
-        brand:req.body.brand,
-        price:req.body.price,
-        category:req.body.category,
-        countInStock:req.body.countInStock,
-        rating:req.body.rating,
-        numReviews:req.body.numReviews,
-        isFeatured:req.body.isFeatured
-        },{new:true})
-    .then(product =>{
-        if(product){
+            name: req.body.name,
+            description: req.body.description,
+            ricDescription: req.body.ricDescription,
+            image: req.body.image,
+            brand: req.body.brand,
+            price: req.body.price,
+            category: req.body.category,
+            countInStock: req.body.countInStock,
+            rating: req.body.rating,
+            numReviews: req.body.numReviews,
+            isFeatured: req.body.isFeatured
+        }, {new: true})
+        .then(product => {
+            if (product) {
+                return res.status(200).json({
+                    success: true,
+                    message: 'the product updated',
+                    product: product
+                })
+            } else {
+                return res.status(404).json({
+                    success: false,
+                    message: 'the product not updated'
+                })
+            }
+        }).catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        })
+})
+
+//delete a product
+router.delete('/:id', (req, res) => {
+    Product.findByIdAndRemove(req.params.id).then(product => {
+        if (product) {
             return res.status(200).json({
                 success: true,
-                message:'the product updated',
-                product:product
+                message: 'the product removed'
             })
-        }else{
+        } else {
             return res.status(404).json({
-                success:false,
-                message:'the product not updated'
+                success: false,
+                message: 'the product not removed'
             })
         }
-    }).catch(err =>{
-        res.status(500).json({
+    }).catch(err => {
+        return res.status(400).json({
+            success: false,
             error: err
         })
     })
 })
 
-//delete a product
-router.delete('/:id',(req, res)=>{
-    Product.findByIdAndRemove(req.params.id).then(product =>{
-        if(product){
-            return res.status(200).json({
-                success: true,
-                message:'the product removed'
-            })
-        }else{
-            return res.status(404).json({
-                success:false,
-                message:'the product not removed'
-            })
-        }
-    }).catch(err =>{
-        return res.status(400).json({
-            success: false,
-            error:err
-        })
-    })
-})
-
-router.get('/get/count',async (req,res)=>{
-    const productCount= await Product.countDocuments((count)=>count)
-    if(!productCount){
-        res.status(500).json({success:false})
-    }
-    else{
-        res.send({count:productCount})
+router.get('/get/count', async (req, res) => {
+    const productCount = await Product.countDocuments((count) => count)
+    if (!productCount) {
+        res.status(500).json({success: false})
+    } else {
+        res.send({count: productCount})
     }
 });
 
-router.get('/',async (req,res)=>{
-    let filter={}
-    if(req.query.category){
-         filter={category:req.query.category.split(',')}
+router.get('/', async (req, res) => {
+    let filter = {}
+    if (req.query.category) {
+        filter = {category: req.query.category.split(',')}
     }
     console.log('filter:' + filter)
-    const productList= await Product.find(filter).populate('category')
-    if(!productList){
-        res.status(500).json({success:false})
-    }
-    else{
-        res.send({prodocts:productList})
+    const productList = await Product.find(filter).populate('category')
+    if (!productList) {
+        res.status(500).json({success: false})
+    } else {
+        res.send({prodocts: productList})
     }
 });
-
-router.put('/images-list/:id',upload.array('images',7) ,async(req,res)=>{
-    if(!mongoose.isValidObjectId(req.params.id)){
-        return res.status(500).send('invalid productId ')
-    }
-    files = req.files
-    if(!files){
-        res.status(400).send('insert the images')
-    }
-    const originalPath = `${req.protocol}://${req.get('host')}/uploads`
-    let imgPaths = []
-    files.map( file =>{
-        imgPaths.push(`${originalPath}/${file.filename}`)
-    })
-
-    const product=await Product.findOneAndUpdate(
-        req.params.id,
-        {
-        images:imgPaths
-        },{new:true}).then(product =>{
-            if(product){
-                return res.status(200).json({
-                    success: true,
-                    message:'the product updated',
-                    product:product
-                })
-            }else{
-                return res.status(404).json({
-                    success:false,
-                    message:'the product not updated'
-                })
-            }
-        }).catch(err =>{
-            res.status(500).json({
-                error: err
-            })
-        })
-
-})
-module.exports= router
+module.exports = router
